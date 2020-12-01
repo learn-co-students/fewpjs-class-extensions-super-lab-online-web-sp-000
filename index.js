5a0f7dc694aae46fa4f74220e506dda0b2295fd6
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-12-01 18:52:28 +0000

    Done.

--- index.js
+++ index.js
@@ -28,14 +28,40 @@ class Tree{
 // > Deciduous trees shed their leaves annually.
 
 class Deciduous extends Tree{
-  constructor(name, super.species){
-    this.name = name;
-    this.species = species;
+  constructor(species, name){
+    super(species)
+    this.name = name
   }
 
   static definition(){
     return (
-      super.definition() + `Deciduous trees shed their leaves annually.`;
+      super.definition() + ` Deciduous trees shed their leaves annually.`
   )
   }
+}
+
+// - An `Evergreen` instance takes two parameters, `species` and `name`. Use
+//   `super()` in the constructor to use the parent class constructor to assign
+//   `species`, then assign `name` to a property in the `Evergreen`
+//   constructor
+// - Create a static method, `definition()`, that uses `super` to access
+//   `definition()` from `Tree` and add the following to the provide a specific
+//   definition for `Evergreen`:
+//
+// > Evergreens keep their leaves all year round.
+
+class Evergreen extends Tree{
+  constructor(species, name){
+    super(species)
+    this.name = name
+  }
+
+
+    static definition(){
+      return (
+        super.definition() + ` Evergreens keep their leaves all year round.`
+        //another way of writing the above:
+        //return `${super.definition()} Evergreens keep their leaves all year round.`
+    )
+    }
 }
