7033f7873a34f39b2708f87399969b14cb07234e
Carrie Mathieu <carrieburks@gmail.com>
2021-03-22 12:52:50 -0400

    Done.

--- index.js
+++ index.js
@@ -4,17 +4,28 @@ class Tree {
     }
 
     static definition() {
-        return "A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves"
+        return "A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves."
     }
 }
 
 class Deciduous extends Tree {
-    constructor (name, species) {
+    constructor (species, name) {
         super(species)
         this.name = name
     }
 
     static definition() {
-        return Tree.definition()
+        return Tree.definition() + " " + "Deciduous trees shed their leaves annually."
+    }
+}
+
+class Evergreen extends Tree {
+    constructor (species, name){
+        super(species)
+        this.name = name
+    }
+
+    static definition() {
+        return Tree.definition() + " " + "Evergreens keep their leaves all year round."
     }
 }
